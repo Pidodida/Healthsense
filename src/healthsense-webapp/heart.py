3a59b0f5ce10143fa4609bdc72abbbacc4990351
Pidodida <91645251+Pidodida@users.noreply.github.com>
2023-11-18 19:04:13 +0800

    Update heart.py

--- src/healthsense-webapp/heart.py
+++ src/healthsense-webapp/heart.py
@@ -2,7 +2,7 @@ import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-something_model = pickle.load(open('heart.sav', 'rb'))
+something_model = pickle.load(open('src/healthsense-webapp/heart.sav', 'rb'))
 with st.sidebar:
     
     selected = option_menu('Heart Disease Prediction System', 
